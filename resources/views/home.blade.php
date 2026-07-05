4fc71183bbe370c5a06cb93decade799dee35edf
IannxGusion <ardiansyahrian771@gmail.com>
2026-07-05 23:14:14 +0700

    home and welcome

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-[#0f1117] text-white flex items-center justify-center min-h-screen">
+
+<h1 class="text-5xl font-bold">
+    Welcome To VYXIAN Library
+</h1>
+
+</body>
+</html>
